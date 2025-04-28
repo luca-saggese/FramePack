0235a8311dfa67ab3b150ce5757a7326b6da15b0
Luca Saggese <luca.saggese@gmail.com>
2025-04-28 10:21:09 +0200

    wip

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,4 @@
 docker build -t framepack .
 echo "✅ Build completata!"
 echo "👉 Per eseguire il container usa:"
-echo "docker run --rm -it --gpus all -p 8080:7860 -v /home/lvx/huggingface:/huggingface framepack"
+echo "docker run --rm -it --gpus all -p 8080:8080 -v /home/lvx/huggingface:/huggingface framepack"
